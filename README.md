1d416d089f1dd2bf5294dc6f6e56ed6c6c00d26a
Harsshad Pawar <pawar.harshad186@gmail.com>
2026-08-13 21:51:13 +0400

    Initial commit: NotebookLM demo (Next.js client + Express/Prisma server)

---
+++ README.md
@@ -0,0 +1,21 @@
+Tech Stack
+
+Server :
+1. express
+2. typescript
+3. prisma
+4. postgres
+5. better-auth
+6. openai - vector embeddings
+7. ai-sdk - for chat kindof applicaitons
+8. vectordb - pinecone
+9. cloudinary
+10.Firecrawl - fpr scrapping
+11.tavily - web searching
+12. inngest - bakground processing
+
+Client : 
+1. nextjs
+2. shadcn
+3. tanstack query
+4. zustand
---
+++ server/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import "dotenv/config";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+import cors from "cors";
+import { registerRoutes } from "./routes/index.js";
+import { errorHandler } from "./middleware/error-handler.middleware.js";
+import { inngest } from "./inngest/client.js";
+import { serve } from "inngest/express";
+import {functions} from "./inngest/index.js"
+const app = express();
+const PORT = process.env.PORT;
+const clientUrl = process.env.CLIENT_URL ?? "http://localhost:3000";
+
+
+app.use(
+    cors({
+        origin: clientUrl,
+        credentials: true,
+    }),
+);
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/health", (_req, res) => {
+    res.json({ status: "ok" });
+});
+
+
+registerRoutes(app);
+
+app.use(errorHandler)
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on port 8081");
+});
